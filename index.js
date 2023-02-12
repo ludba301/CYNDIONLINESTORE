bf49d70ba5e4c2e54a816041b5512ea5cc48ff11
ludba301 <59377949+ludba301@users.noreply.github.com>
2023-02-12 10:38:55 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+// object {}
+// asigning a variable let
+// constant variable const
+//string, number, boolean, undifined
+// dot notation p.property
+// brackcet notaion []
+// array [] a data structure used to represent a list of items
+//functions (){function body}
+//a function is a set of statement that either perform a task or calculate and return a value
+// calling the function functionname();
+// always end a line of code with semi coloumn
