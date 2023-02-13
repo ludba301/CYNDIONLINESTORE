e75f519d7c68c2ee2fb6c84803213283190260ff
ludba301 <59377949+ludba301@users.noreply.github.com>
2023-02-13 16:33:15 +0000

    update

--- index.js
+++ index.js
@@ -9,4 +9,3 @@
 //a function is a set of statement that either perform a task or calculate and return a value
 // calling the function functionname();
 // always end a line of code with semi coloumn
-
